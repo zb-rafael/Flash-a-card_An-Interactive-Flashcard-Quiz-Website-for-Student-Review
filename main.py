46735cd5c119954da9c62557b9adf2658f567d04
zb-rafael <zbrafael@carc.pshs.edu.ph>
2026-03-05 19:03:25 +0800

    Added instructions (BY LAURINO)

---
+++ main.py
@@ -0,0 +1,15 @@
+# FLASH-A-CARD
+# RAFAEL, ZYA KYNDER TARHATA B.
+# LAURINO, JUSTINE MATTHEW B.
+# DE GUZMAN, ALESSANDRA FIONA M.
+
+def show_instructions():
+    print("----- INSTRUCTIONS -----")
+    print("Flash-a-card helps you review Grade 8 subjects using flashcards !")
+    print("How to use:")
+    print("1. Choose a topic.")
+    print("2. Answer questions.")
+    print("3. Explanations appear after answering.")
+    print("4. View session summary at the end.")
+    print("5. Start optional Pomodoro timer for extra focus.")
+    input("Press ENTER to return to menu.")
